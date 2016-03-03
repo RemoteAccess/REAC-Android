0fcaff87efa7d01befd054dc08591a610e2f43d7
scopeInfinity <gagan1kumar@ymail.com>
2016-03-03 16:51:45 +0530

    Init Repo

---
+++ app/src/main/java/in/ac/iitp/remoteaccess/activity/Home.java
@@ -0,0 +1,28 @@
+package in.ac.iitp.remoteaccess.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import in.ac.iitp.remoteaccess.R;
+
+public class Home extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Intent in = getIntent();
+        if(!in.hasExtra(LoginActivity.INTENT_EMAIL))
+        {
+            Toast.makeText(this, "No Data",Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        TextView tv_user = (TextView) findViewById(R.id.user);
+        tv_user.setText(in.getStringExtra(LoginActivity.INTENT_EMAIL));
+    }
+}
---
+++ app/src/main/java/in/ac/iitp/remoteaccess/utils/Constants.java
@@ -0,0 +1,8 @@
+package in.ac.iitp.remoteaccess.utils;
+
+/**
+ * Created by scopeinfinity on 3/3/16.
+ */
+public class Constants {
+
+}
